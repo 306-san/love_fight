22aec51ec3aaa6f341d23875ca0ac52d7237381a
Yuta Ide <yIde4949@gmail.com>
2016-01-17 11:38:03 +0900

    画像を追加しました

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( *.png )
 Rails.application.config.assets.precompile += %w( people.png )
+Rails.application.config.assets.precompile += %w( *.jpg )
